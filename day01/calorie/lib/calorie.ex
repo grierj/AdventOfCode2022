11cd3d10badf636abe13ddab858655d7299e5618
Grier Johnson <grierj@gmail.com>
2022-12-01 23:11:31 -0800

    output of mix new

---
+++ day01/calorie/lib/calorie.ex
@@ -0,0 +1,18 @@
+defmodule Calorie do
+  @moduledoc """
+  Documentation for Calorie.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Calorie.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ day01/calorie/test/calorie_test.exs
@@ -0,0 +1,8 @@
+defmodule CalorieTest do
+  use ExUnit.Case
+  doctest Calorie
+
+  test "greets the world" do
+    assert Calorie.hello() == :world
+  end
+end

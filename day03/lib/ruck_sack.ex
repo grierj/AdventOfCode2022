41f4e0aaf44701bc3b1445bb2afb18be180c0b94
Grier Johnson <grierj@gmail.com>
2022-12-03 11:58:44 -0800

    trying out mix for day 3

---
+++ day03/lib/ruck_sack.ex
@@ -0,0 +1,18 @@
+defmodule RuckSack do
+  @moduledoc """
+  Documentation for `RuckSack`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RuckSack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

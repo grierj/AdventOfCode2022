66296696bd305ef9308e2ad14bfa9e93e93d8a87
Grier Johnson <grierj@gmail.com>
2022-12-03 12:35:37 -0800

    I need to think about the score lookup table

--- day03/lib/ruck_sack.ex
+++ day03/lib/ruck_sack.ex
@@ -1,18 +1,58 @@
 defmodule RuckSack do
   @moduledoc """
   Documentation for `RuckSack`.
+
+  Givent `data/input.txt` take the contents, line by line and:
+  - split them in half (they will be different lengths)
+  - find any shared items (represented by the same letter, case-sensitive)
+  - score the shared items based on a-zA-Z translating to 1-52
+  - sum all the scores from every line
   """
 
   @doc """
-  Hello world.
+  Run the program
 
   ## Examples
 
-      iex> RuckSack.hello()
-      :world
+      iex> RuckSack.run()
+      :integer()
 
   """
-  def hello do
-    :world
+  def run do
+    File.read!(Path.expand(Path.join("data","input.txt")))
+    |> String.split("\n")
+    |> Enum.map(fn(x) -> split_lines(x) end)
+
+    0
+  end
+
+  @doc """
+  Given a line, split it in half and return the two halves as a tuple
+
+  ## Examples
+
+      iex> RuckSack.split_lines("abcd")
+      {"ab", "cd"}
+
+  """
+  def split_lines(line) do
+    len = String.length(line)
+    {
+      String.slice(line, 0, div(len, 2)),
+      String.slice(line, div(len, 2), len - 1)
+    }
+  end
+
+  @doc """
+  """
+  def find_common(item_pair) do
+    MapSet.intersection(
+      MapSet.new(String.graphemes(elem(item_pair, 0)))
+      MapSet.new(String.graphemes(elem(item_pair, 1)))
+    )
+  end
+
+  def gen_score(item_list) do
+
   end
 end

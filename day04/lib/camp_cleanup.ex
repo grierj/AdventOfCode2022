4577d45ec932116ebdad76f6953162e8391739c5
Grier Johnson <grierj@gmail.com>
2022-12-04 15:39:27 -0800

    Correct part 1 answer

--- day04/lib/camp_cleanup.ex
+++ day04/lib/camp_cleanup.ex
@@ -15,6 +15,30 @@ defmodule CampCleanup do
   """
 
   def run do
-
+    IO.puts(
+      File.read!(Path.expand(Path.join("data", "input.txt")))
+      |> String.split("\n")
+      |> Enum.reject(fn x -> x == "" end)
+      |> Enum.map(fn x -> String.split(x, ",") end)
+      |> Enum.map(fn x -> find_subset(x) end)
+      |> Enum.sum()
+    )
   end
+
+  def find_subset(pair) do
+    [first_range, second_range] = Enum.map(pair, fn x -> str_range_to_map_set(x) end)
+    cond do
+      MapSet.subset?(first_range, second_range) -> 1
+      MapSet.subset?(second_range, first_range) -> 1
+      true -> 0
+    end
+  end
+
+  @spec str_range_to_map_set(binary) :: MapSet
+  def str_range_to_map_set(string) do
+    [range_start, range_end] = String.split(string, "-")
+    |> Enum.map(fn x -> String.to_integer(x) end)
+    MapSet.new(Range.new(range_start, range_end))
+  end
+
 end

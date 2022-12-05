920c23acb5864bf517028fa4702b182dbe043fc5
Grier Johnson <grierj@gmail.com>
2022-12-05 11:30:50 -0800

    `mix new` and the day 5 input.txt file

---
+++ day05/lib/supply_stacks.ex
@@ -0,0 +1,18 @@
+defmodule SupplyStacks do
+  @moduledoc """
+  Documentation for `SupplyStacks`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SupplyStacks.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

0f9eaff41eab1156660e672a9ef1b526f4dc90fd
Grier Johnson <grierj@gmail.com>
2022-12-06 14:43:00 -0800

    mix new + input.txt

---
+++ day06/lib/tuning_trouble.ex
@@ -0,0 +1,18 @@
+defmodule TuningTrouble do
+  @moduledoc """
+  Documentation for `TuningTrouble`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TuningTrouble.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

e5820596cba3d7ab4cc37dce5fd2763a28ea433b
Grier Johnson <grierj@gmail.com>
2022-12-06 16:18:25 -0800

    Part 1 solution for day 6

--- day06/lib/tuning_trouble.ex
+++ day06/lib/tuning_trouble.ex
@@ -1,18 +1,51 @@
 defmodule TuningTrouble do
   @moduledoc """
   Documentation for `TuningTrouble`.
+
+  Given a string of characters, locate the position of the first character
+  after a series of 4 characters where none repeat.  Examples:
+
+  - bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 5
+  - nppdvjthqldpwncqszvftbrmjlhg: first marker after character 6
+  - nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 10
+  - zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 11
+
+  We'll just split our string into graphemes and look back 4 characters for duplicates.
   """
 
   @doc """
-  Hello world.
+  Run the program
 
-  ## Examples
-
-      iex> TuningTrouble.hello()
-      :world
+  ```
+  $ iex -S mix
+  iex> TuningTrouble.run
+  4
+  :ok
+  ```
 
   """
-  def hello do
-    :world
+  def run do
+    charlist =
+      File.read!(Path.expand(Path.join("data", "input.txt")))
+      |> String.graphemes()
+      |> Enum.with_index()
+
+    IO.puts(
+      Enum.reduce_while(charlist, 0, fn {_, i}, _ ->
+        if i > 3 do
+          (Enum.slice(charlist, (i - 3)..i)
+           |> Enum.map(fn {e, _} -> e end)
+           |> list_has_dupes?() && {:cont, 0}) || {:halt, i + 1}
+        else
+          {:cont, 0}
+        end
+      end)
+    )
+  end
+
+  def list_has_dupes?(list) do
+    IO.inspect(list)
+    IO.inspect(Enum.uniq(list))
+    length(list) != length(Enum.uniq(list))
   end
 end
